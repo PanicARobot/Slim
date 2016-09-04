938888fd023b67a274b29821ef7794dcbdb27bdb
Kristiyan Tsaklev <cuki@cukii.tk>
2016-09-05 02:43:09 +0300

    Have meaningful logger

--- src/SDLogDriver.cpp
+++ src/SDLogDriver.cpp
@@ -2,19 +2,80 @@
 
 #include <SD.h>
 
-void read_file(const char* filename)
+#define LOG_FILENAME    "LOG"
+#define BEGIN_LIFE_BYTE 'B'
+#define BEGIN_DATA_BYTE 'b'
+
+__LOG_DATA log_data_pack;
+
+File f;
+
+char* const data_begin = (char*)(&log_data_pack); // Yes, yes, I know
+int data_length = sizeof(log_data_pack);
+
+void initLogger()
 {
-	File f = SD.open(filename);
-	if(!f) Serial.println("No such file");
-	else while(f.available())
-		Serial.write(f.read());
-	f.close();
+	f = SD.open(LOG_FILENAME, FILE_WRITE);
+	f.write(BEGIN_LIFE_BYTE);
+	f.flush();
 }
 
-void append_file(const char* filename, const char* entry)
+void logDataPack()
 {
-	File f = SD.open(filename, FILE_WRITE);
-	if(!f) Serial.println("Can not write to file");
-	f.println(entry);
+	f.write(BEGIN_DATA_BYTE);
+	f.write(data_begin, data_length);
+	f.flush();
+}
+
+void dumpLog()
+{
 	f.close();
+
+	f = SD.open(LOG_FILENAME, FILE_READ);
+
+	while(f.peek() >= 0)
+	{
+		char c = f.read();
+		if(c == BEGIN_LIFE_BYTE)
+		{
+			Serial.println("UNIT BOOTUP");
+		}
+		else if(c == BEGIN_DATA_BYTE)
+		{
+			for(int i = 0; i < data_length; ++i)
+				data_begin[i] = f.read();
+
+			Serial.print(log_data_pack.timestamp); Serial.print("ms: ");
+
+			Serial.print("Acc (");
+			Serial.print(log_data_pack.acc_x); Serial.print(", ");
+			Serial.print(log_data_pack.acc_y); Serial.print(", ");
+			Serial.print(log_data_pack.acc_z); Serial.print(")\t");
+
+			Serial.print("Gyro (");
+			Serial.print(log_data_pack.gyro_x); Serial.print(", ");
+			Serial.print(log_data_pack.gyro_y); Serial.print(", ");
+			Serial.print(log_data_pack.gyro_z); Serial.print(")\t");
+
+			Serial.print("Ahrs (");
+			Serial.print(log_data_pack.ahrs_x); Serial.print(", ");
+			Serial.print(log_data_pack.ahrs_y); Serial.print(", ");
+			Serial.print(log_data_pack.ahrs_z); Serial.print(")\t");
+
+			Serial.print("Encoders (");
+			Serial.print(log_data_pack.leftSpeed); Serial.print(", ");
+			Serial.print(log_data_pack.rightSpeed); Serial.print(")\n");
+		}
+		else
+		{
+			Serial.println("Log file is corrupt");
+			break;
+		}
+	}
+
+	f.close();
+
+	SD.remove(LOG_FILENAME);
+
+	initLogger();
 }
--- src/SDLogDriver.h
+++ src/SDLogDriver.h
@@ -1,7 +1,20 @@
 #ifndef __SD_LOG_DRIVER_H
 #define __SD_LOG_DRIVER_H
 
-void read_file(const char* filename);
-void append_file(const char*, const char*);
+#include <cstdint>
+
+struct __LOG_DATA {
+	uint32_t timestamp;
+	float acc_x, acc_y, acc_z;
+	float gyro_x, gyro_y, gyro_z;
+	float ahrs_x, ahrs_y, ahrs_z;
+	float leftSpeed, rightSpeed;
+};
+
+extern __LOG_DATA log_data_pack;
+
+void initLogger();
+void logDataPack();
+void dumpLog();
 
 #endif // __SD_LOG_DRIVER_H
