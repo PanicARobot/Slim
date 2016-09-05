c61037e808e552778a6d64005213c406acfc1d0d
Kristiyan Tsaklev <cuki@cukii.tk>
2016-09-05 14:39:47 +0300

    Add planar acceleration module, need to test it

---
+++ src/PlanarAccelerationModule.cpp
@@ -0,0 +1,57 @@
+#include "PlanarAccelerationModule.h"
+
+#include "OrientationSensorsWrapper.h"
+#include "DualEncoderDriver.h"
+#include "MotorDriver.h"
+
+#define MOTORS_CALIBRATE_SPEED 80, 80
+
+const uint32_t MICROS_PER_SEC = 1000000;
+const int CALIBRATION_FREQUENCY = 250;
+
+void PlanarAcceleration::calibrate()
+{
+	acc_offset_x = 0;
+	acc_offset_y = 0;
+
+	int samples_count = 0;
+
+	uint32_t start_micros = micros();
+	uint32_t last_micros;
+
+	float last_left_speed = 0;
+	float last_right_speed = 0;
+
+	setMotors(MOTORS_CALIBRATE_SPEED);
+
+	while(true)
+	{
+		uint32_t current_micros = micros();
+		if(current_micros - start_micros > MICROS_PER_SEC / 3)
+			return;
+
+		if(current_micros - last_micros < MICROS_PER_SEC / CALIBRATION_FREQUENCY)
+			continue;
+
+		float left_speed = leftEncoder.getSpeed()
+		float right_speed = rightEncoder.getSpeed();
+
+		float acceleration = (left_speed + right_speed - last_left_speed - last_right_speed) / 2;
+
+		acc_offset_x += position.getAccX();
+		acc_offset_y += position.getAccY() - acceleration;
+
+		++samples_count;
+
+		last_left_speed = left_speed;
+		last_right_speed = right_speed;
+	}
+
+	acc_offset_x /= samples_count;
+	acc_offset_y /= samples_count;
+
+	Serial.print(acc_offset_x); Serial.print(" ");
+	Serial.print(acc_offset_y); Serial.println("");
+
+	setMotors(0, 0);
+}
---
+++ src/PlanarAccelerationModule.h
@@ -0,0 +1,13 @@
+#ifndef __PLANAR_ACCELERATION_MODULE_H
+#define __PLANAR_ACCELERATION_MODULE_H
+
+class PlanarAcceleration {
+	private:
+		float acc_offset_x;
+		float acc_offset_y;
+
+	public:
+		void calibrate();
+};
+
+#endif // __PLANAR_ACCELERATION_MODULE_H
