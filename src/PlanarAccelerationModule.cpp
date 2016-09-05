391ee190e575e29f7609fe21ebdd2c2b22116135
Kristiyan Tsaklev <cuki@cukii.tk>
2016-09-05 22:34:09 +0300

    Get acceleration vector from encoders

--- src/PlanarAccelerationModule.cpp
+++ src/PlanarAccelerationModule.cpp
@@ -1,64 +1,21 @@
 #include "PlanarAccelerationModule.h"
 
-#include "OrientationSensorsWrapper.h"
 #include "DualEncoderDriver.h"
-#include "MotorDriver.h"
 
 #include "constants.h"
 
-void PlanarAcceleration::calibrate()
-{
-	acc_offset_x = 0;
-	acc_offset_y = 0;
-
-	int samples_count = 0;
-
-	uint32_t start_micros = micros();
-	uint32_t last_micros = start_micros - MICROS_PER_SECOND;
-
-	float last_left_speed = 0;
-	float last_right_speed = 0;
-
-	setMotors(MOTORS_CALIBRATE_SPEED);
-
-	while(true)
-	{
-		uint32_t current_micros = micros();
-		if(current_micros - start_micros > MICROS_PER_SECOND / 3)
-			break;
-
-		if(current_micros - last_micros < MICROS_PER_SECOND / CALIBRATION_FREQUENCY)
-			continue;
-
-		leftEncoder.updateSpeed();
-		rightEncoder.updateSpeed();
-
-		float left_speed = leftEncoder.getSpeed();
-		float right_speed = rightEncoder.getSpeed();
-
-		float acceleration = (left_speed + right_speed - last_left_speed - last_right_speed) / 2;
-
-		acc_offset_x += position.getAccX();
-		acc_offset_y += position.getAccY() - acceleration;
-
-		++samples_count;
-
-		last_left_speed = left_speed;
-		last_right_speed = right_speed;
-
-		last_micros = current_micros;
-	}
-
-	acc_offset_x /= samples_count;
-	acc_offset_y /= samples_count;
-
-	setMotors(0, 0);
-}
-
 void PlanarAcceleration::update()
 {
-	acc_x = position.getAccX();
-	acc_y = position.getAccY();
+	static constexpr float d = 85; // mm
+
+	float vl = leftEncoder.getSpeed();
+	float al = leftEncoder.getAcc();
+
+	float vr = rightEncoder.getSpeed();
+	float ar = rightEncoder.getAcc();
+
+	acc_x = (vl * vl - vr * vr) / (4 * d);
+	acc_y = (al + ar) / 2;
 }
 
 PlanarAcceleration planarAcceleration;
--- src/PlanarAccelerationModule.h
+++ src/PlanarAccelerationModule.h
@@ -5,25 +5,14 @@
 
 class PlanarAcceleration {
 	private:
-		float acc_offset_x;
-		float acc_offset_y;
-
 		float acc_x;
 		float acc_y;
 
 	public:
-		void calibrate();
-
 		void update();
 
-		inline float getAcc()
-		{
-			return sqrt(acc_x * acc_x + acc_y * acc_y);
-		}
-		inline float getAngle()
-		{
-			return atan2(-acc_x, acc_y) * 180 / M_PI;
-		}
+		inline float getX() { return acc_x; }
+		inline float getY() { return acc_y; }
 };
 
 extern PlanarAcceleration planarAcceleration;
