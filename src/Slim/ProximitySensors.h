b6c9f694031d8bf44e548186871d8b836b837355
Kristiyan Tsaklev <cuki@cukii.tk>
2016-10-13 18:39:21 +0300

    Update

--- src/Slim/ProximitySensors.h
+++ src/Slim/ProximitySensors.h
@@ -1,17 +1,9 @@
 #ifndef __PROXIMITY_SENSORS_DRIVER_H
 #define __PROXIMITY_SENSORS_DRIVER_H
 
-#include <stdint.h>
+#include <cstdint>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void initProximitySensors(void);
+void initProximitySensors();
 void readProximitySensors(uint8_t&, uint8_t&);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // __PROXIMITY_SENSORS_DRIVER_H
