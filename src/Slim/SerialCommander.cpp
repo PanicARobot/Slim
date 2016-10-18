7b8399520c6904ffe1d1be40dc342d5359d96195
Kristiyan Tsaklev <cuki@cukii.tk>
2016-10-18 21:52:15 +0300

    Split serial commander

---
+++ src/Slim/SerialCommander.cpp
@@ -0,0 +1,49 @@
+#include "SerialCommander.h"
+
+#include "SDLogDriver.h"
+#include "MotorDriver.h"
+
+#include <Wire.h>
+
+void getSerialCommand()
+{
+	if(Serial.available() == 0) return;
+
+	static int left = 0;
+	static int right = 0;
+
+	char cmd = Serial.read();
+
+	if(cmd == 'l' || cmd == 'r' || cmd == 'b')
+	{
+		int speed = 0;
+		if(Serial.available() > 0)
+		{
+			int dir = 1;
+			if(Serial.peek() == '-')
+			{
+				dir = -1;
+				Serial.read();
+			}
+			while(Serial.available() > 0)
+			{
+				char digit = Serial.read();
+				speed = speed * 10 + (digit - '0');
+			}
+			speed *= dir;
+		}
+
+		if(cmd == 'l' || cmd == 'b') left = speed;
+		if(cmd == 'r' || cmd == 'b') right = speed;
+
+		setMotors(left, right);
+	}
+	else if(cmd == 'd')
+	{
+		dumpLog();
+	}
+	else if(cmd == 'e')
+	{
+		dropLog();
+	}
+}
---
+++ src/Slim/SerialCommander.h
@@ -0,0 +1,14 @@
+#ifndef __SERIAL_COMMANDER_H
+#define __SERIAL_COMMANDER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void getSerialCommand(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __SERIAL_COMMANDER_H
