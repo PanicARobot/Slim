e627e051eab23d8de8172b0da0be400fc4d1c1fa
Kristiyan Tsaklev <cuki@cukii.tk>
2016-11-19 16:04:16 +0200

    Add planar speed module

---
+++ src/Slim/PlanarSpeedModule.cpp
@@ -0,0 +1,15 @@
+#include "PlanarSpeedModule.hpp"
+
+#include "PlanarAccelerationModule.hpp"
+#include "DualEncoderDriver.hpp"
+
+static Point3D<double> speed_vector;
+
+void initPlanarSpeed(OrientationSensors& position) {
+	calibrateGravity(position);
+}
+
+void updatePlanarSpeed(OrientationSensors& position) {
+	updatePlanarAcceleration(position);
+	speed_vector += acceleration_vector;
+}
---
+++ src/Slim/PlanarSpeedModule.hpp
@@ -0,0 +1,9 @@
+#ifndef __PLANAR_SPEED_MODULE_H
+#define __PLANAR_SPEED_MODULE_H
+
+#include "OrientationSensorsWrapper.hpp"
+
+void initPlanarSpeed(OrientationSensors&);
+void updatePlanarSpeed(OrientationSensors&);
+
+#endif // __PLANAR_SPEED_MODULE_H
