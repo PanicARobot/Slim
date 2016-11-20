9b982908cdd210f4a2cc5b83e5f55841c1afb463
Kristiyan Tsaklev <cuki@cukii.tk>
2016-11-20 11:34:03 +0200

    Playing with planar

--- src/Slim/planar/PlanarAccelerationModule.cpp
+++ src/Slim/planar/PlanarAccelerationModule.cpp
@@ -17,8 +17,11 @@ void calibrateGravity(OrientationSensors& position) {
 	uint8_t start_micros = micros();
 	uint8_t last_micros = start_micros;
 
-	while(true) { // time for calibration
-		uint32_t current_micros = micros();
+		acceleration_vector += {
+			position.getAccX(),
+			position.getAccY(),
+			position.getAccZ()
+		};
 
 		if(current_micros - start_micros > CALIBRATION_TIME)
 			break;
@@ -41,12 +44,42 @@ void calibrateGravity(OrientationSensors& position) {
 }
 
 void updatePlanarAcceleration(OrientationSensors& position) {
-	acceleration_vector.x = position.getAccX(); // CHECK THESE
-	acceleration_vector.y = position.getAccY(); // CHECK THESE
-	acceleration_vector.z = position.getAccZ();
+	/*
+	   acceleration_vector = {
+	   position.getAccX(),
+	   position.getAccY(),
+	   position.getAccZ()
+	   };
 
-	// Aaand CHECK THESE
-	acceleration_vector.x -= sin(position.getPitch()) * cos(position.getRoll()) * gravity;
-	acceleration_vector.y += sin(position.getRoll()) * gravity;
-	acceleration_vector.z -= cos(position.getPitch()) * cos(position.getRoll()) * gravity;
+	   float roll = position.getRoll();
+	   float pitch = position.getPitch();
+
+	   acceleration_vector = {
+	   cos(pitch)  * acceleration_vector.x + sin(pitch) * sin(roll) * acceleration_vector.y + sin(pitch) * cos(roll) * acceleration_vector.z,
+	   0           * acceleration_vector.x +              cos(roll) * acceleration_vector.y -              sin(roll) * acceleration_vector.z,
+	   -sin(pitch) * acceleration_vector.x + cos(pitch) * sin(roll) * acceleration_vector.y + cos(pitch) * cos(roll) * acceleration_vector.z
+	   };
+	   */
+
+	acceleration_vector = {
+		position.getAccX(),
+		position.getAccY(),
+		position.getAccZ()
+	};
+
+	float roll = -position.getRoll();
+	float pitch = -position.getPitch();
+
+	/*
+	acceleration_vector -= {
+		+ sin(pitch) * cos(roll) * gravity,
+		-              sin(roll) * gravity,
+		+ cos(pitch) * cos(roll) * gravity
+	};
+	*/
+	acceleration_vector -= {
+		             sin(pitch) * gravity,
+		-sin(roll) * cos(pitch) * gravity,
+		cos(roll)  * cos(pitch) * gravity
+	};
 }
