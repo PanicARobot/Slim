eb7fd032e285772c7f974a82f6045cf29479f43c
Kristiyan Tsaklev <cuki@cukii.tk>
2016-11-20 13:49:04 +0200

    Add point rotations

--- src/Slim/utility/Point3D.hpp
+++ src/Slim/utility/Point3D.hpp
@@ -1,6 +1,8 @@
 #ifndef __POINT_3D_H
 #define __POINT_3D_H
 
+#include <cmath>
+
 template<typename T>
 struct Point3D {
 	T x, y, z;
@@ -27,6 +29,28 @@ struct Point3D {
 		x *= div;
 		y *= div;
 		z *= div;
+	}
+
+	inline void rotatePR(float pitch, float roll) {
+		float cp = cos(pitch), cr = cos(roll);
+		float sp = sin(pitch), sr = sin(roll);
+
+		float nx =  cp * x + sp * sr * y + sp * cr * z;
+		float ny =           cr * y -           sr * z;
+		float nz = -sp * x + cp * sr * y - cp * cr * z;
+
+		x = nx; y = ny; z = nz;
+	}
+
+	inline void rotateYPR(float yaw, float pitch, float roll) {
+		float cy = cos(yaw), cp = cos(pitch), cr = cos(roll);
+		float sy = sin(yaw), sp = sin(pitch), sr = sin(roll);
+
+		float nx = cy * cp * x + (cy * sp * sr - sy * cr) * y + (cy * sp * cr + sy * sr) * z;
+		float ny = sy * cp * x + (sy * sp * sr + cy * cr) * y + (sy * sp * cr - cy * sr) * z;
+		float nz =    - sp * x +       cp * sr            * y -       cp * cr            * z;
+
+		x = nx; y = ny; z = nz;
 	}
 };
 
